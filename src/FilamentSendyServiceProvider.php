98bcad5ea232363e30ca77fd53b06a6f6b708dff
henk-n <81883380+henk-n@users.noreply.github.com>
2026-04-27 17:40:10 +0000

    Fix styling

--- src/FilamentSendyServiceProvider.php
+++ src/FilamentSendyServiceProvider.php
@@ -8,13 +8,13 @@ use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
+use Henkn\FilamentSendy\Commands\FilamentSendyCommand;
+use Henkn\FilamentSendy\Testing\TestsFilamentSendy;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Henkn\FilamentSendy\Commands\FilamentSendyCommand;
-use Henkn\FilamentSendy\Testing\TestsFilamentSendy;
 
 class FilamentSendyServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,13 +13,13 @@ use Filament\Schemas\SchemasServiceProvider;
 use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
+use Henkn\FilamentSendy\FilamentSendyServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Henkn\FilamentSendy\FilamentSendyServiceProvider;
 
 class TestCase extends Orchestra
 {
